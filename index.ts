cce584753c290bc2857e4b9bd15719141fe2a347
Marvin <85842735+Madu-de@users.noreply.github.com>
2022-10-14 14:57:41 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,12 +2,18 @@
 // Code by Madu https://github.com/madu-de GNU Affero General Public License v3.0 license
 // #########################################################################################
 
-const HexToDez: string = 'beef'; // Hier eine Zahl, die von Hexadezimal zu Dezimal gerechnet werden soll.
-const DezToHex: number = 234; // Hier eine Zahl, die von Dezimal zu Hexadezimal gerechnet werden soll.
-const BinToDez: string = '01010101001010101001010101010101010101010101010'; // Hier eine Zahl, die von Binär zu Dezimal gerechnet werden soll.
-const DezToBin: number = 234; // Hier eine Zahl, die von Dezimal zu Binär gerechnet werden soll.
-const HexToBin: string = '19856398756Fe'; // Hier eine Zahl, die von Hexadezimal zu Binär gerechnet werden soll.
-const BinToHex: string = '1001'; // Hier eine Zahl, die von Binär zu Hexadezimal gerechnet werden soll.
+const HexToDez: string = 'BEEFFFFFFFFFFF'; // Hier eine Zahl, die von Hexadezimal zu Dezimal gerechnet werden soll.
+const DezToHex: number = 666; // Hier eine Zahl, die von Dezimal zu Hexadezimal gerechnet werden soll.
+const BinToDez: string = '00100100101100111'; // Hier eine Zahl, die von Binär zu Dezimal gerechnet werden soll.
+const DezToBin: number = 900; // Hier eine Zahl, die von Dezimal zu Binär gerechnet werden soll.
+const HexToBin: string = 'FFFFFFFFF'; // Hier eine Zahl, die von Hexadezimal zu Binär gerechnet werden soll.
+const BinToHex: string = '00100100101100111'; // Hier eine Zahl, die von Binär zu Hexadezimal gerechnet werden soll.
+const DezToOkt: number = 23542452457; // Hier eine Zahl, die von Denizmal zu Oktal gerechnet werden soll.
+const HexToOkt: string = 'FF'; // Hier eine Zahl, die von hexadezimal zu Oktal gerechnet werden soll.
+const BinToOkt: string = '111'; // Hier eine Zahl, die von Binär zu Oktal gerechnet werden soll.
+const OktToDez: string = '16725371214124'; // Hier eine Zahl, die von Oktal in Dezimal gerechnet werden soll.
+const OktToHex: string = '200'; // Hier eine Zahl, die von Oktal in Hexadezimal gerechnet werden soll.
+const OktToBin: string = '200'; // Hier eine Zahl, die von Oktal in Binär gerechnet werden soll.
 
 console.log('HexToDez says: ' + hexToDez(HexToDez));
 console.log('DezToHex says: ' + dezToHex(DezToHex));
@@ -15,6 +21,12 @@ console.log('BinToDez says: ' + binToDez(BinToDez));
 console.log('DezToBin says: ' + dezToBin(DezToBin));
 console.log('HexToBin says: ' + hexToBin(HexToBin));
 console.log('BinToHex says: ' + binToHex(BinToHex));
+console.log('DezToOkt says: ' + dezToOkt(DezToOkt));
+console.log('HexToOkt says: ' + hexToOkt(HexToOkt));
+console.log('BinToOkt says: ' + binToOkt(BinToOkt));
+console.log('OktToDez says: ' + oktToDez(OktToDez));
+console.log('OktToHex says: ' + oktToHex(OktToHex));
+console.log('OktToBin says: ' + oktToBin(OktToBin));
 
 function hexToDez(input: string): any {
 
@@ -171,4 +183,58 @@ function binToHex(input: string): string {
     }
 
     return output;
+}
+
+function dezToOkt(input: number): number {
+    let output: number = 0;
+    let rests: number[] = [];
+    while (input > 0) {
+        rests.push(input % 8);
+        input = Math.floor(input / 8);
+    }
+
+    rests = rests.reverse();
+
+    output = parseInt(rests.join(''));
+
+    return output;
+}
+
+function hexToOkt(input: string): number {
+    let dez: number = hexToDez(input);
+    let output: number = dezToOkt(dez);
+    return output;
+}
+
+function binToOkt(input: string): number {
+    let dez: number = binToDez(input);
+    let output: number = dezToOkt(dez);
+    return output;
+}
+
+function oktToDez(input: string): number {
+    let output: number = 0;
+    let count: number = 0;
+    let zahl: number;
+
+    for (let i = input.length; i > 0; i--) {
+        let element: string = input.charAt(i - 1);
+        zahl = parseInt(element);
+        output += zahl * Math.pow(8, count);
+        count++;
+    }
+
+    return output;
+}
+
+function oktToHex(input: string): string {
+    let dez: number = oktToDez(input);
+    let hex: string = dezToHex(dez);
+    return hex;
+}
+
+function oktToBin(input: string): string {
+    let dez: number = oktToDez(input);
+    let bin: string = dezToBin(dez);
+    return bin;
 }
